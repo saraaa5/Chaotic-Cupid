6559b60398610c5c5019add38d6592a081d913fa
Sara Cvjetkovic <saracvjetkovic2005@gmail.com>
2026-05-31 18:34:11 +0200

    feature: server logic - hub, services and model

---
+++ CupidonServer/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace CupidonServer.Models
+{
+    public class Person
+    {
+        public string Username { get; set; }
+        public string Town { get; set; }
+        public int Age { get; set; }
+        public string PhoneNum { get; set; }
+        public string ConnectionId { get; set; }
+        public bool WaitForResponse { get; set; }
+        public HashSet<string> Blocked { get; set; }
+    }
+}
